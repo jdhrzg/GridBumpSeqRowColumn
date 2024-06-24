af8ab051703bafdd1bd35a4ee52b20fb2d035626
Jerod Herzog <jdhrzg@gmail.com>
2024-06-24 07:47:36 -0500

    Working on consolidating all logic into Shared class.  Genericizing "find values" logic.

--- GridBumpSeqRowColumn/Commands/MyCommand.cs
+++ GridBumpSeqRowColumn/Commands/MyCommand.cs
@@ -8,28 +8,28 @@ namespace GridBumpSeqRowColumn
     {
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            var doc = await VS.Documents.GetActiveDocumentViewAsync();
-            if (doc != null)
-            {
-                SnapshotSpan span = doc.TextView.Selection.SelectedSpans[0];
-                var selectedText = span.GetText();
-                var formattedSelectedText = selectedText.Replace(" ", "");
+            //var doc = await VS.Documents.GetActiveDocumentViewAsync();
+            //if (doc != null)
+            //{
+            //    SnapshotSpan span = doc.TextView.Selection.SelectedSpans[0];
+            //    var selectedText = span.GetText();
+            //    var formattedSelectedText = selectedText.Replace(" ", "");
 
-                Regex regex = new Regex(@"((?i)grid\.row=""(.*?)""(?-i))");
+            //    Regex regex = new Regex(@"((?i)grid\.row=""(.*?)""(?-i))");
 
-                var matches = regex.Matches(formattedSelectedText);
+            //    var matches = regex.Matches(formattedSelectedText);
 
-                foreach (Match match in matches)
-                {
-                    var quoteStartIdx = match.Value.IndexOf('"');
-                    var quoteEndIdx = match.Value.LastIndexOf('"');
+            //    foreach (Match match in matches)
+            //    {
+            //        var quoteStartIdx = match.Value.IndexOf('"');
+            //        var quoteEndIdx = match.Value.LastIndexOf('"');
 
-                    if (quoteStartIdx != -1 && quoteEndIdx != -1)
-                    {
-                        var value = match.Value.Substring((quoteStartIdx + 1), quoteEndIdx - (quoteStartIdx + 1));
-                    }
-                }
-            }
+            //        if (quoteStartIdx != -1 && quoteEndIdx != -1)
+            //        {
+            //            var value = match.Value.Substring((quoteStartIdx + 1), quoteEndIdx - (quoteStartIdx + 1));
+            //        }
+            //    }
+            //}
         }
     }
 }
--- GridBumpSeqRowColumn/Shared/Shared.cs
+++ GridBumpSeqRowColumn/Shared/Shared.cs
@@ -1,16 +1,63 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GridBumpSeqRowColumn
 {
     internal class Shared
     {
-        public static void SomeMethod()
+        public enum FindValueTypes
         {
+            GridRow = 0,
+            GridColumn = 1,
+        }
 
+        //TODO: Consider other formats like <Grid.Row>1</Grid.Row>
+        public static Regex GridRowRegex = new Regex(@"((?i)grid\.row=""(.*?)""(?-i))");
+        public static Regex GridColumnRegex = new Regex(@"((?i)grid\.column=""(.*?)""(?-i))");
+
+        //TODO: Left off - working on this method, then work on doing something to those values (inc, dec, seq), after that put the values back into the selected span
+        public static async List<int> GetValuesFromSelection(FindValueTypes findValueType)
+        {
+            var valuesFromSelection = new List<int>();
+
+            var doc = await VS.Documents.GetActiveDocumentViewAsync();
+            if (doc != null)
+            {
+                string selectedText = doc.TextView.Selection.SelectedSpans[0].GetText();
+                var formattedSelectedText = selectedText.Replace(" ", "");
+
+                Regex regex = null;
+
+                if (findValueType == FindValueTypes.GridRow)
+                    regex = GridRowRegex;
+                else if (findValueType == FindValueTypes.GridColumn)
+                    regex = GridColumnRegex;
+
+                var matches = regex.Matches(formattedSelectedText);
+
+                foreach (Match match in matches)
+                {
+                    var quoteStartIdx = match.Value.IndexOf('"');
+                    var quoteEndIdx = match.Value.LastIndexOf('"');
+
+                    if (quoteStartIdx != -1 && quoteEndIdx != -1)
+                    {
+                        int intValue;
+
+                        var success = int.TryParse(match.Value.Substring((quoteStartIdx + 1), quoteEndIdx - (quoteStartIdx + 1)), out intValue);
+
+                        if (success)
+                        {
+                            valuesFromSelection.Add(intValue);
+                        }
+                    }
+                }
+            }
         }
     }
 }
